dfb55e5a602ded882acca24820f50508d2c32e03
CXG <chasecastora@gmail.com>
2022-07-27 13:35:41 -0400

    Bug fixes & changes

--- src/events/ready.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk')
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		client.user.setActivity("linkie.gg", { type: "WATCHING" })
-		console.log(chalk.gray('[INFO] ') + chalk.white('Bot logged in successfully under ') + chalk.cyan(chalk.bold(`${client.user.tag}`)))
-	},
-};
